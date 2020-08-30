abe4f9eae25f4b586c0829fa28270ed72c1f2431
Liam Curran <liamcurran2016@outlook.com>
2020-08-30 12:22:56 +0100

    Cleaned up files

--- main.tf
+++ main.tf
@@ -9,65 +9,6 @@ provider "aws" {
     version = "~> 3.0"
 }
 
-resource "aws_instance" "example" { 
-    ami = "ami-0c55b159cbfafe1f0" 
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.SG1.id]
-    user_data = <<-EOF
-                #!/bin/bash
-                echo "Hello World, from Terraform up and running" > index.html
-                nohup busybox httpd -f -p ${var.server_port} &
-                EOF
-    tags = { 
-        Name = "terraform-example" 
-    } 
-} 
-
-
-/*
-# Grab the default VPC
-data "aws_vpc" "default-vpc" {
-    default = true
-}
-
-# Grab the subnet id's from default VPC
-data "aws_subnet_ids" "subnet_ids" {
-    vpc_id = data.aws_vpc.default-vpc.id
-}
-
-# Define the Auto Scaling Group launch configuration
-resource "aws_launch_configuration" "ASG-launch-config" { 
-    image_id = "ami-0c55b159cbfafe1f0" 
-    instance_type = "t2.micro" 
-    security_groups = [aws_security_group.SG1.id]
-
-    user_data = <<-EOF
-                #!/bin/bash
-                echo "Hello World!" > index.html
-                nohup busybox httpd -f -p ${var.server_port} &
-                EOF
-
-# Required when using a launch configuration with an auto scaling group. 
-# https://www.terraform.io/docs/providers/aws/r/launch_configuration.html 
-lifecycle { create_before_destroy = true }
-
-
-} 
-
-# Define the Auto Scaling Group
-resource "aws_autoscaling_group" "ASG1" { 
-    launch_configuration = aws_launch_configuration.ASG-launch-config.name 
-    vpc_zone_identifier = data.aws_subnet_ids.subnet_ids.ids
-    min_size = 2 
-    max_size = 10 
-    tag { 
-         key = "Name" 
-         value = "terraform-asg-example" 
-         propagate_at_launch = true 
-    } 
-} 
-*/
-
 # Define the Security Group
 resource "aws_security_group" "SG1" { 
     name = "terraform-example-sg" 
@@ -79,4 +20,17 @@ resource "aws_security_group" "SG1" {
         } 
 } 
 
-
+# Define the EC2 instance
+resource "aws_instance" "ec2-01" { 
+    ami = "ami-0c55b159cbfafe1f0" 
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.SG1.id]
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello World, from Terraform up and running" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+    tags = { 
+        Name = "terraform-example" 
+    } 
+}
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "ec2_public_ip" { 
-    value = aws_instance.example.public_ip 
+    value = aws_instance.ec2-01.public_ip 
     description = "The public IP address of the web server:" 
 } 
