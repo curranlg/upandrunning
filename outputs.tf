5e612f5d4b9df8db2af68aacb09c21b9e647bd2a
Liam Curran <65068764+curranlg@users.noreply.github.com>
2020-08-30 16:21:00 +0100

    Commented out outputs variable as was for single EC2 instance

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,6 @@
+/* Commented out as moved from single EC2 instance to ASG
 output "ec2_public_ip" { 
     value = aws_instance.ec2-01.public_ip 
     description = "The public IP address of the web server:" 
 } 
+*/
