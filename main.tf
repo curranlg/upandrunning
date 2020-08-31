8d787503a525faf5330c25beeba0d5376c57723b
Liam Curran <65068764+curranlg@users.noreply.github.com>
2020-08-31 13:27:12 +0100

    Added ASG plus ALB

--- main.tf
+++ main.tf
@@ -31,9 +31,10 @@ resource "aws_launch_configuration" "asg-lc01" {
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
     
+    lifecycle {
     # Required when using a launch configuration with an auto scaling group
     # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html 
-    lifecycle {
+    
         create_before_destroy = true
     }
 }
@@ -41,6 +42,9 @@ resource "aws_launch_configuration" "asg-lc01" {
 # Define the Auto Scaling Group (ASG)
 resource "aws_autoscaling_group" "asg01" {
     launch_configuration = aws_launch_configuration.asg-lc01.name
+    vpc_zone_identifier  = data.aws_subnet_ids.vpc_sn_ids.ids
+    target_group_arns = [aws_lb_target_group.alb-tg-01.arn]
+    health_check_type = "ELB"
     min_size = 2
     max_size = 10
 
@@ -48,5 +52,102 @@ resource "aws_autoscaling_group" "asg01" {
         key = "Name"
         value = "terraform-asg-example"
         propagate_at_launch = true
+    }
+
+    lifecycle {
+    # Required when using a launch configuration with an auto scaling group
+    # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html 
+    
+        create_before_destroy = true
+    }
+}
+
+# Grad reference to the default VPC
+data "aws_vpc" "default_vpc" {
+    default = true
+}
+# Grab the subnet id's from the default VPC
+data "aws_subnet_ids" "vpc_sn_ids" {
+    vpc_id = data.aws_vpc.default_vpc.id
+}
+
+# Create the ALB across all default VPC subnets
+resource "aws_lb" "alb01" {
+    name = "terraform-asg-example"
+    load_balancer_type = "application"
+    subnets = data.aws_subnet_ids.vpc_sn_ids.ids
+    security_groups = [aws_security_group.sg-alb-01.id]
+}
+
+# Create the ALB HTTP listener
+resource "aws_lb_listener" "listener01" {
+    load_balancer_arn = aws_lb.alb01.arn
+    port = 80
+    protocol = "HTTP"
+
+    # By default, return a simple 404 page
+    default_action {
+      type = "fixed-response"
+
+      fixed_response {
+        content_type = "text/plain"
+        message_body = "404: page not found"
+        status_code = "404"
+      }
+    }
+}
+
+# Create Security Group for the ALB
+resource "aws_security_group" "sg-alb-01" {
+    name = "terraform-example-alb"
+
+    # Allow inbound HTTP from the Internet
+    ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        description = "Allow HTTP traffic from the Internet"
+  }
+    # Allow all outbound requests
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create the target group
+resource "aws_lb_target_group" "alb-tg-01" {
+    name = "terraform-asg-tg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default_vpc.id
+
+    health_check {
+      path = "/"
+      protocol = "HTTP"
+      matcher = "200"
+      interval = 15
+      timeout = 3
+      healthy_threshold = 2
+      unhealthy_threshold = 2
+    }
+}
+
+# Create the ALB Listener Rule 
+resource "aws_lb_listener_rule" "alb-lr-01" {
+    listener_arn = aws_lb_listener.listener01.arn
+    priority = 100
+
+    condition {
+        path_pattern {
+          values = ["*"]
+        }
+    }
+    action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.alb-tg-01.arn
     }
 }
