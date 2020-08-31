323dab9619f4a858f2ae9e383681711a0b5ba422
Liam Curran <65068764+curranlg@users.noreply.github.com>
2020-08-31 13:28:00 +0100

    Modified output to show DNS name of ALB

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,4 @@
-/* Commented out as moved from single EC2 instance to ASG
-output "ec2_public_ip" { 
-    value = aws_instance.ec2-01.public_ip 
-    description = "The public IP address of the web server:" 
+output "alb_dns_name" { 
+    value = aws_lb.alb01.dns_name
+    description = "The domain name of the load balancer" 
 } 
-*/
